24029e0c4c385b6513e65fc23be6f789c553c044
Vladimir Brasil <vladimir.brasil@gmail.com>
2013-02-11 15:12:34 -0200

    Configura ambiente inicial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Claropj2::Application.config.secret_token = '4770400e617ea0919228d7e8aa3bab13b784b8b14a8d1108b810b5cc2c9ba996efe9bf91e4584e11eb48a028a68a230c8a4bdef021f1aa2f9e95d3419912e3e0'
